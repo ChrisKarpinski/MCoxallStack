56183b6a49fb8b161802d49cddc3b61b5bb57b03
ChrisKarpinski <christopher.karpinski@ocsbstudent.ca>
2016-10-03 14:46:21 -0400

    Update MCoxallClass.java

--- MCoxallClass.java
+++ MCoxallClass.java
@@ -7,15 +7,46 @@
  ****************************************************************************/
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MCoxallStack {
+public class MCoxallStack<Item> {
 
-	private ArrayList<String> stackArray = new ArrayList<String>();
+	private List<Item> _stackArray = new ArrayList<Item>();
 	
-	public void push (String someString) {
+	public void push (Item someString) {
 		
-		stackArray.add(someString);
+		_stackArray.add(someString);
 		
 	}
 	
+	public void pop () {
+		
+		
+		if (_stackArray.size() > 0) {
+			peek();
+		_stackArray.remove(_stackArray.size()-1);
+		
+		}
+		else { 
+			System.err.println("The stack is empty, cannot pop");
+		}
+		
+	}
+	
+	public void peek () {
+		if (_stackArray.size() > 0) {
+		Item lastElement = _stackArray.get(_stackArray.size()-1);
+		System.out.println(lastElement);
+		}
+		else {
+			System.err.println("The stack is empty, cannot peek");
+		}
+		
+	}
+	
+	public void clear () {
+		
+		_stackArray.clear();
+		
+	}
 }
